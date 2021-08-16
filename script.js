6d11d7321dbe121e78b1e5fed5500fd40087b542
quyt0 <dphuc2304@gmail.com>
2021-08-17 05:39:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,57 @@
-$(document).ready(function () {
-var pop_count = 0;
-var pressed = false;
-pop_count = parseInt(document.cookie.toString().replaceAll("pop_count=", ""));
-if (isNaN(pop_count)) {
-    pop_count = 0;
-}
-document.getElementsByClassName('counter')[0].innerText = pop_count;
+$(document).ready(function() {
+    var pop_count = 0;
+    var pressed = false;
+    pop_count = parseInt(document.cookie.toString().replaceAll("pop_count=", ""));
+    if (isNaN(pop_count)) {
+        pop_count = 0;
+    }
+    document.getElementsByClassName('counter')[0].innerText = pop_count;
 
-document.onkeydown = function() {
-    down_key();
-};
+    if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+        $(document).on('touchstart', function() {
+            down_key();
+        });
 
-document.onkeyup = function () {
-    up_key();
-};
+        $(document).on('touchend', function() {
+            up_key();
+        });
+    } else {
+        document.onkeydown = function() {
+            down_key();
+        }
+        ;
 
-$(document).mousedown(function() {
-    down_key();
-});
+        document.onkeyup = function() {
+            up_key();
+        }
+        ;
 
-$(document).mouseup(function() {
-    up_key();
-});
+        $(document).mousedown(function() {
+            down_key();
+        });
 
-$(document).on('touchstart', function() {
-    down_key();
-});
+        $(document).mouseup(function() {
+            up_key();
+        });
+    }
 
-$(document).on('touchend', function() {
-    up_key();
-});
-
-
-function down_key() {
-    if (pressed == false) {
-        var audio = new Audio('sound.mp3');
-        audio.play();
-        pop_count +=1;
-        document.cookie = 'pop_count=' + pop_count;
-        document.getElementsByClassName('counter')[0].innerText = pop_count;
-        document.getElementById('img-main').style="background-image: url(img/popcat-open.png);"
-        pressed = true;
-    };
-};
-
-function up_key() {
-    document.getElementById('img-main').style="background-image: url(img/popcat-default.png);"
-    pressed = false;
-};
+    function down_key() {
+        if (pressed == false) {
+            var audio = new Audio('sound.mp3');
+            audio.play();
+            pop_count += 1;
+            document.cookie = 'pop_count=' + pop_count;
+            document.getElementsByClassName('counter')[0].innerText = pop_count;
+            document.getElementById('img-main').style = "background-image: url(img/popcat-open.png);"
+            pressed = true;
+        }
+        ;
+    }
+    ;
+    function up_key() {
+        document.getElementById('img-main').style = "background-image: url(img/popcat-default.png);"
+        pressed = false;
+    }
+    ;
 
 });
